0d85d101c1b4b47ef964e6c5ee225160497af628
Jeremy Unck <jeremy@Jeremys-MacBook-Pro.local>
2017-02-17 22:41:02 -0800

    assignment 5 work

--- assignment5/caesar.cpp
+++ assignment5/caesar.cpp
@@ -1,11 +1,44 @@
-#include "caesarcipher.hpp"
+#include "caesar.hpp"
 #include "cipher.hpp"
-#include <string>
+
+CaesarCipher::CaesarCipher() : Cipher() {
+
+}
+
+CaesarCipher::~CaesarCipher() {
+
+}
 
 std::string CaesarCipher::encrypt( std::string &text ) {
-  
+  std::string output = text;
+  std::string::size_type len = output.length();
+  for (int i = 0; i != len; ++i) {
+    if (output[i] >= 'a' && output[i] <= 'z' - shift) {
+      output[i] = output[i] + shift;
+    } else if (output[i] >= 'A' && output[i] <= 'Z' - shift) {
+      output[i] = output[i] + shift;
+    } else if (output[i] > 'z' - shift && output[i] <= 'z') {
+      output[i] = 'a' + ((output[i] + shift) - 'z');
+    } else if (output[i] > 'Z' - shift && output[i] <= 'Z') {
+      output[i] = 'A' + ((output[i] + shift) - 'Z');
+    }
+  }
+  return output;
 }
 
 std::string CaesarCipher::decrypt( std::string &text ) {
-
+  std::string output = text;
+  std::string::size_type len = output.length();
+  for (int i = 0; i != len; ++i) {
+    if (output[i] >= 'a' + shift && output[i] <= 'z') {
+      output[i] = output[i] - shift;
+    } else if (output[i] >= 'A' + shift && output[i] <= 'Z') {
+      output[i] = output[i] - shift;
+    } else if (output[i] > 'a' && output[i] <= 'a' + shift) {
+      output[i] = 'z' - (output[i] - shift);
+    } else if (output[i] > 'Z' - shift && output[i] <= 'Z') {
+      output[i] = 'Z' + (output[i] - shift);
+    }
+  }
+  return output;
 }
--- assignment5/caesar.hpp
+++ assignment5/caesar.hpp
@@ -1,20 +1,18 @@
 #ifndef CAESARCIPHER_HPP_
 #define CAESARCIPHER_HPP_
 
+#include "cipher.hpp"
+
 class CaesarCipher : public Cipher {
 private:
-  
+  int shift = 1;
 public:
   CaesarCipher();
-  ~CaesarCipher();
-
-	std::string encrypt( std::string &text ) = 0;
-	std::string decrypt( std::string &text ) = 0;
+  virtual ~CaesarCipher();
+	virtual std::string encrypt( std::string &text );
+	virtual std::string decrypt( std::string &text );
 
 };
-
-
-
 
 
 #endif
